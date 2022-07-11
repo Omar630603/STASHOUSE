79a1a008945fa3eab5ec56e12153a6992231b3cc
Omar630603 <omar.yem1111@gmail.com>
2022-07-11 18:24:47 +0700

    Edit text

--- resources/views/storage_owner/dashboard.blade.php
+++ resources/views/storage_owner/dashboard.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __("Storage Owner's Dashboard:") }}
+            {{ __("Storage Owner's Dashboard") }}
         </h2>
         <x-message-success />
         <x-message-error />
