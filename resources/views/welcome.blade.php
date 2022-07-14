8b725cbbe1d856f7a6b346f696b11858b90462c1
Omar630603 <omar.yem1111@gmail.com>
2022-07-14 22:15:28 +0700

    Landing page section one

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,8 +1,68 @@
 <x-guest-layout>
-    <div class="relative flex items-top justify-center min-h-screen sm:items-center py-4 sm:pt-0">
-        <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
-            <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
-                <x-application-logo />
+    <div class="container px-6 py-16 mx-auto">
+        <div class="lg:flex">
+            <div class="w-full lg:w-1/2 h-[500px] items-center flex">
+                <div class="max-w-fit">
+                    <h1 class="text-2xl font-semibold text-[#4E1D65] tracking-wide lg:text-5xl font-bold">
+                        Temukan
+                        Unit Penyimpanan yang Sesuai Denganmu</h1>
+                    <p class="mt-9 lg:text-2xl text-black">Solusimu untuk menyimpan barang dengan aman</p>
+                    <div class="flex mt-9 gap-4">
+                        <div class="flex gap-2 items-center">
+                            <svg width="50" height="50" viewBox="0 0 50 50" fill="none"
+                                xmlns="http://www.w3.org/2000/svg">
+                                <circle cx="25" cy="25" r="25" fill="#FFE8F2" />
+                                <g clip-path="url(#clip0_449_24)">
+                                    <path
+                                        d="M24.8662 27.5874C26.8162 27.5874 28.3682 25.6105 28.3682 23.1717C28.3682 20.7328 26.7874 18.7559 24.8662 18.7559C22.9161 18.7559 21.3641 20.7328 21.3641 23.1717C21.3336 25.6088 22.9164 27.5874 24.8662 27.5874ZM26.5645 28.9461H23.1746C20.5082 28.9461 18.3478 31.481 18.3478 34.606C18.3478 35.2344 18.7792 35.7396 19.3125 35.7396H30.4266C30.9599 35.7396 31.3913 35.2344 31.3913 34.606C31.3913 31.481 29.231 28.9461 26.5645 28.9461ZM31.3913 20.794C32.892 20.794 34.1087 19.2731 34.1087 17.3972C34.1087 15.5214 32.8927 14.0005 31.3913 14.0005C29.8906 14.0005 28.6739 15.5214 28.6739 17.3972C28.6739 19.2731 29.8899 20.794 31.3913 20.794ZM20.2466 23.1717C20.2466 22.9403 20.28 22.7203 20.3013 22.4962C19.9307 22.2418 19.5231 22.1527 19.0917 22.1527H16.9918C15.3397 22.1527 14 23.9275 14 26.1141C14 26.5514 14.2679 26.9081 14.5985 26.9081H21.375C20.6814 25.8976 20.2466 24.6026 20.2466 23.1717ZM18.3478 20.794C19.8485 20.794 21.0652 19.2731 21.0652 17.3972C21.0652 15.5214 19.8492 14.0005 18.3478 14.0005C16.8471 14.0005 15.6304 15.5214 15.6304 17.3972C15.6304 19.2731 16.8471 20.794 18.3478 20.794ZM32.7466 22.1527H30.6461C30.2113 22.1527 29.801 22.2816 29.428 22.5025C29.4484 22.7259 29.4823 22.9424 29.4823 23.1717C29.4823 24.603 29.0482 25.898 28.356 26.9081H35.1393C35.4708 26.9081 35.7391 26.5514 35.7391 26.1141C35.7391 23.9275 34.4008 22.1527 32.7466 22.1527Z"
+                                        fill="#E52878" />
+                                </g>
+                                <defs>
+                                    <clipPath id="clip0_449_24">
+                                        <rect width="21.7391" height="21.7391" fill="white"
+                                            transform="translate(14 14)" />
+                                    </clipPath>
+                                </defs>
+                            </svg>
+                            <p class="font-semibold text-base">5000+ Pengguna</p>
+                        </div>
+                        <div class="flex gap-2 items-center">
+                            <svg width="50" height="50" viewBox="0 0 50 50" fill="none"
+                                xmlns="http://www.w3.org/2000/svg">
+                                <circle cx="25" cy="25" r="25" fill="#FFE8F2" />
+                                <g clip-path="url(#clip0_449_23)">
+                                    <path
+                                        d="M14 34.7199V21.2731C14 20.1649 14.5411 19.1629 15.3665 18.751L24.4654 14.2016C24.7249 14.0721 25.0143 14.0721 25.2738 14.2016L34.3736 18.751C35.199 19.1629 35.7391 20.1649 35.7391 21.2731V34.7199C35.7391 35.2847 35.3757 35.739 34.9239 35.739H33.2935C32.8417 35.739 32.4783 35.2847 32.4783 34.7199V23.4725C32.4783 22.7592 31.9925 22.1138 31.3913 22.1138H18.3478C17.7466 22.1138 17.2609 22.7592 17.2609 23.4725V34.7199C17.2609 35.2847 16.8957 35.739 16.4457 35.739H14.8152C14.3651 35.739 14 35.2847 14 34.7199ZM19.163 35.739C18.7113 35.739 18.3478 35.2847 18.3478 34.7199V32.3422H31.3913V34.7199C31.3913 35.2847 31.0279 35.739 30.5761 35.739H19.163ZM18.3478 28.2661H31.3913V30.9835H18.3478V28.2661ZM18.3478 23.5107H31.3913V26.9074H18.3478V23.5107Z"
+                                        fill="#E52878" />
+                                </g>
+                                <defs>
+                                    <clipPath id="clip0_449_23">
+                                        <rect width="21.7391" height="21.7391" fill="white"
+                                            transform="translate(14 14)" />
+                                    </clipPath>
+                                </defs>
+                            </svg>
+                            <p class="font-semibold text-base">500+ Unit Penyimpanan</p>
+                        </div>
+                    </div>
+                    <div class="mt-9">
+                        <a href=""
+                            class="bg-[#F8C35B] hover:bg-[#F1A613] text-[$3F1652] font-bold py-4 px-10 rounded-full">
+                            Sewa Sekarang
+                        </a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="w-full lg:mt-0 lg:w-1/2 relative">
+                <img class="lg:max-w-2xl absolute top-0 right-0 animate-floatX"
+                    src="{{ asset('storage/images/stockImages/hero1.svg') }}" alt="">
+                <img class="lg:max-w-2xl absolute top-[250px] right-0 animate-floatX"
+                    src="{{ asset('storage/images/stockImages/hero2.svg') }}" alt="">
+                <img class="lg:max-w-2xl absolute top-[250px] right-[300px] animate-floatY"
+                    src="{{ asset('storage/images/stockImages/hero3.svg') }}" alt="">
+                <img class="lg:max-w-2xl absolute top-[350px] right-[100px] z-[-1] animate-floatX"
+                    src="{{ asset('storage/images/stockImages/hero4.svg') }}" alt="">
             </div>
         </div>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,54 @@
-const defaultTheme = require('tailwindcss/defaultTheme');
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+        "./storage/framework/views/*.php",
+        "./resources/views/**/*.blade.php",
     ],
 
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Nunito', ...defaultTheme.fontFamily.sans],
+                sans: ["Nunito", ...defaultTheme.fontFamily.sans],
+            },
+            keyframes: {
+                floatY: {
+                    "0%": {
+                        shadow: "0 5px 15px 0px rgba(0,0,0,0.6)",
+                        transform: "translatey(0px)",
+                    },
+                    "50%": {
+                        shadow: "0 25px 15px 0px rgba(0,0,0,0.2)",
+                        transform: "translatey(-20px)",
+                    },
+                    "100%": {
+                        shadow: "0 5px 15px 0px rgba(0,0,0,0.6)",
+                        transform: "translatey(0px)",
+                    },
+                },
+                floatX: {
+                    "0%": {
+                        shadow: "0 5px 15px 0px rgba(0,0,0,0.6)",
+                        transform: "translatex(0px)",
+                    },
+                    "50%": {
+                        shadow: "0 25px 15px 0px rgba(0,0,0,0.2)",
+                        transform: "translatex(-20px)",
+                    },
+                    "100%": {
+                        shadow: "0 5px 15px 0px rgba(0,0,0,0.6)",
+                        transform: "translatex(0px)",
+                    },
+                },
+            },
+            animation: {
+                floatY: "floatY 3s ease-in-out infinite",
+                floatX: "floatX 3s ease-in-out infinite",
             },
         },
     },
 
-    plugins: [require('@tailwindcss/forms')],
+    plugins: [require("@tailwindcss/forms")],
 };
